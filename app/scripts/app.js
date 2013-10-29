91f8711a41ad9195379848b1717a127ebf64faa4
antoine <duvillierantoine@gmail.com>
2013-10-29 23:27:28 +0100

    manipulate template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,12 @@
 'use strict';
 
-angular.module('resumeApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var allinone = angular.module('allinone', []);
+
+allinone.config(function ($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'views/main.html',controller: 'PhoneListCtrl'});
+  $routeProvider.when('/twitter', {templateUrl: 'views/twitter.html', controller: 'twitter'});
+  $routeProvider.when('/google+', {templateUrl: 'views/google.html', controller: 'google'});
+  $routeProvider.when('/github', {templateUrl: 'views/github.html', controller: 'github'});
+  $routeProvider.when('/gravatar', {templateUrl: 'views/gravatar.html', controller: 'gravatar'});
+  $routeProvider.otherwise({redirectTo: '/'});
+});
