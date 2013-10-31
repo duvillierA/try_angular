679551189d8be303cf75ff142bba4e1350b8b11c
Antoine Duvillier <aduvillier@viadeoteam.com>
2013-10-31 18:29:35 +0100

    multiple view + mock Rest Api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,23 @@
 'use strict';
 
-var phoneApp = angular.module('phoneApp', ['ngRoute','phoneControllers']);
+var phoneApp = angular.module('phoneApp', ['ngRoute','phoneControllers','phonecatFilters']);
 
-phoneApp.config(['$routeProvider', function($routeProvider) {
+phoneApp.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider.when('/twitter', {templateUrl: 'views/twitter.html', controller: 'twitter'});
 	$routeProvider.when('/google+', {templateUrl: 'views/google.html', controller: 'google'});
 	$routeProvider.when('/github', {templateUrl: 'views/github.html', controller: 'github'});
 	$routeProvider.when('/gravatar', {templateUrl: 'views/gravatar.html', controller: 'gravatar'});
 
-	$routeProvider.when('/phones', {templateUrl: 'views/phone-list.html',controller: 'PhoneListCtrl'});
-	$routeProvider.when('/phones/:phoneId', {templateUrl: 'views/phone-detail.html', controller: 'PhoneDetailCtrl'});
+	$routeProvider.when('/phones', {templateUrl: '/views/phone-list.html',controller: 'PhoneListCtrl'});
+	$routeProvider.when('/phones/:phoneId', {templateUrl: '/views/phone-detail.html', controller: 'PhoneDetailCtrl'});
 
 	$routeProvider.otherwise({redirectTo: '/phones'});
+
+	$locationProvider.html5Mode(true);
 }]);
+
+angular.module('phonecatFilters', []).filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,7 +11,13 @@ phoneControllers.controller('PhoneListCtrl',['$scope', '$http',
 
   }]);
 
-phoneControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-	function($scope, $routeParams) {
-		$scope.phoneId = $routeParams.phoneId;
+phoneControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams','$http',
+	function($scope, $routeParams, $http) {
+		$http.get('/phoneDetail.json').success(function(data) {
+			$scope.phone = data;
+			$scope.mainImageUrl = data.images[0];
+		});
+		$scope.setImage = function(img) {
+			$scope.mainImageUrl = img;
+		}
 }]);
